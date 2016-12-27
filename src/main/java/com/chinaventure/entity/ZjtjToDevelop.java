83394d4eef2d57812859850ca5eb7b5a930b7ccb
dongyq <953125376@qq.com>
2016-12-28 01:03:26 +0800

    //cmmit ZjtjToDevelop.java

---
+++ src/main/java/com/chinaventure/entity/ZjtjToDevelop.java
@@ -0,0 +1,8 @@
+package com.chinaventure.entity;
+
+/**
+ * Created by 95312 on 2016/12/28.
+ */
+public class ZjtjToDevelop {
+    //直接由local/develop分支先merger 上origin/develop， 再push到origin/develop【merger环节是必须的】
+}
